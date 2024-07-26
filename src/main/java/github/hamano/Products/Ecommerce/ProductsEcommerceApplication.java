b6d3d40f369af58b3af4b417370c534d6dd1b4c1
Ayumi Hamano <98706488+AyuHamano@users.noreply.github.com>
2024-07-26 17:03:10 -0300

    configuração inicial do projeto

---
+++ src/main/java/github/hamano/Products/Ecommerce/ProductsEcommerceApplication.java
@@ -0,0 +1,13 @@
+package github.hamano.Products.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/hamano/Products/Ecommerce/ProductsEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package github.hamano.Products.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
